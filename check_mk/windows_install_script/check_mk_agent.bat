2add0bf695f55891ea24801abc913a94804ebce1
Robin Schneider <robin.schneider@hamcos.de>
2014-06-11 14:00:46 +0200

    Added windows install script.

---
+++ check_mk/windows_install_script/check_mk_agent.bat
@@ -0,0 +1,20 @@
+if exist "%programfiles%\check_mk\check_mk_agent.exe" goto ende
+if exist "%programfiles(x86)%\check_mk\check_mk_agent.exe" goto ende
+
+.\check-mk-agent-1.2.4p3.exe /S
+
+net stop Check_MK_Agent
+
+if exist "%programfiles(x86)%" goto x64
+copy .\config.ini "%programfiles%\check_mk\config.ini" /y
+netsh advfirewall firewall add rule name="Check_MK_Agent" dir=in action=allow program="%programfiles%\check_mk\check_mk_agent.exe" enable=yes
+net start Check_MK_Agent
+goto ende
+
+:x64
+copy .\config.ini "%programfiles(x86)%\check_mk\config.ini" /y
+netsh advfirewall firewall add rule name="Check_MK_Agent" dir=in action=allow program="%programfiles(x86)%\check_mk\check_mk_agent.exe" enable=yes
+net start Check_MK_Agent
+goto ende
+
+:ende
