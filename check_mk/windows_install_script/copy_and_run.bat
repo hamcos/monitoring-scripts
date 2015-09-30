e409edef148e18a02d0c02741d0bf8b59371bfdf
Robin Schneider <robin.schneider@hamcos.de>
2015-09-30 17:13:36 +0200

    Updated copy script.

--- check_mk/windows_install_script/copy_and_run.bat
+++ check_mk/windows_install_script/copy_and_run.bat
@@ -1,6 +1,11 @@
 @echo off
+
+rem @author Robin Schneider <robin.schneider@hamcos.de>
+rem @company hamcos IT Service GmbH http://www.hamcos.de
+rem @license GPLv3 <https://www.gnu.org/licenses/gpl-3.0.html>
+
 rem needed because cmd is incapable of executing the install script from an UNC path.
 
-xcopy "\\WEDERTVSRV008\mon" C:\_service_it\mon /D /E /C /R /H /I /K /Y
+xcopy "\\source\mon" C:\_service_it\mon /D /E /C /R /H /I /K /Y
 cd C:\_service_it\mon
 .\check_mk_agent.bat
