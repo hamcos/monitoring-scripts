f14404235a4eedac730eb55210e8ad57a74b5c80
Robin Schneider <robin.schneider@hamcos.de>
2015-10-28 11:41:47 +0100

    Fixed up agent install script for Windows.
    Fixed:
    * Ensure that script is running as "root".
    * Fixed problem where batch script startet as "root" change directory to system32
    * Made compatible with i386 architecture.
    * Improved logging and user interaction :)

--- check_mk/windows_install_script/check_mk_agent.bat
+++ check_mk/windows_install_script/check_mk_agent.bat
@@ -1,3 +1,5 @@
+@echo off
+
 rem @author Alexander Zell <alexander.zell@hamcos.de>
 rem @maintainer Robin Schneider <robin.schneider@hamcos.de>
 rem @company hamcos IT Service GmbH http://www.hamcos.de
@@ -6,26 +8,56 @@ rem To download the setup file, run https://github.com/hamcos/monitoring-scripts
 
 rem This script is only needed if you do not use configuration management (which you should use).
 
-if exist "%programfiles%\check_mk\check_mk_agent.exe" goto ende
-if exist "%programfiles(x86)%\check_mk\check_mk_agent.exe" goto ende
+if exist "%programfiles%\check_mk\check_mk_agent.exe" goto installed
+if exist "%programfiles(x86)%\check_mk\check_mk_agent.exe" goto installed
 
-.\install_agent-64.exe install
-rem .\install_agent-64.exe /S
+rem Check for root permissions {{{
+rem http://stackoverflow.com/a/11995662
+echo Administrative permissions required. Detecting permissions ...
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Success: Administrative permissions confirmed.
+) else (
+    echo Failure: Current permissions inadequate.
+    echo Please execute with administrative permissions.
+    pause
+    exit
+)
+rem }}}
 
+rem Switch to `dirname $0` {{{
+rem Windows hack to execute all following instruction in context of the directory form where the script is located.
+rem https://social.technet.microsoft.com/Forums/scriptcenter/en-US/360ff40b-6a2b-4651-9a3e-360e803d8ffb/executing-batch-file-as-administrator-changes-working-directory?forum=ITCG
+@setlocal enableextensions
+@cd /d "%~dp0"
+echo Executing script in context of %cd%
+rem }}}
+
+echo Install agent without user interaction.
+.\check_mk_agent.msi /passive
+
+echo Ensure that the agent is stopped.
 net stop Check_MK_Agent
 
+echo Add firewall rule to allow external access to the agent. Note that this will add a new rule each time it is executed.
+netsh advfirewall firewall add rule name="Check_MK_Agent" dir=in action=allow protocol=TCP localport=6556 enable=yes
+
 if exist "%programfiles(x86)%" goto x64
+echo Install on x86-32 architecture.
 mkdir "%programfiles%\check_mk"
 copy .\check_mk.ini "%programfiles%\check_mk\check_mk.ini" /y
-netsh advfirewall firewall add rule name="Check_MK_Agent" dir=in action=allow protocol=TCP localport=6556 enable=yes
 net start Check_MK_Agent
 goto ende
 
 :x64
+echo Install on x86-64 architecture.
 mkdir "%programfiles(x86)%\check_mk"
 copy .\check_mk.ini "%programfiles(x86)%\check_mk\check_mk.ini" /y
-netsh advfirewall firewall add rule name="Check_MK_Agent" dir=in action=allow protocol=TCP localport=6556 enable=yes
 net start Check_MK_Agent
 goto ende
 
+:installed
+echo Agent was already installed.
+
 :ende
+pause
